36d5c1c898eac2a106c9b507dba09733a19a69ac
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-12 20:27:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ if __name__ == "__main__" :
     plugins = dict(root="plugins")
     Warrior = Clinton("@SimpleUploaderBot",
         bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
+        api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins
     )
